cb6a4a4a7dfdcd0baf5ebcc8938bf9bb85f63a0e
Dawid Urbaniak <udawid00@gmail.com>
2023-11-20 14:46:02 +0100

    Updating Object State Immutably

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSqure(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBorad = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBorad[rowIndex][colIndex] = "X";
+      return updatedBorad;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSqure(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
