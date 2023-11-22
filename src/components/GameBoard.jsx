123fcc4b2dacff02746582ca3a100f5acb97b633
Dawid Urbaniak <udawid00@gmail.com>
2023-11-22 16:20:34 +0100

    Final Polishing & Improving Components

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,33 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSqure(rowIndex, colIndex) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedBorad = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedBorad[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBorad;
-  //   });
-
-  //   onSelectSquare();
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
